2cc936f881b5df71f0de2e6769119fb8424829a4
Cintia <cintiabw1995@gmail.com>
2021-03-15 22:34:26 -0300

    estilo formulario

--- resources/views/articulos/formulario-modificacion.blade.php
+++ resources/views/articulos/formulario-modificacion.blade.php
@@ -3,6 +3,65 @@
 <head>
 	<title>Editar Artículo</title>
 	<meta charset="utf-8">
+    <style>
+        *{box-sizing:border-box;}
+
+form{
+	width:300px;
+	padding:16px;
+	border-radius:10px;
+	margin:auto;
+	background-color:#0221aa;
+    color: #ffffff;
+    
+}
+
+form label{
+	width:72px;
+	font-weight:bold;
+	
+}
+
+form input[type="text"],
+form input[type="number"]{
+	width:100%;
+	padding:3px 10px;
+	border:1px solid #f6f6f6;
+	border-radius:3px;
+	background-color:#f6f6f6;
+	margin:8px 0;
+	
+}
+
+
+#cancel, #ok {
+	padding:8px 16px;
+    margin: 10px;
+	border-radius:5px;
+	color:#0000a7;
+	background-color:#ffffff;
+    font-weight: bold;
+    flex-flow: row nowrap;
+}
+#cancel {
+    color: #ff0000;
+}
+#cancel:hover, #ok:hover{
+	cursor:pointer;
+}
+
+textarea{
+	width:100%;
+	height:100px;
+	border:1px solid #f6f6f6;
+	border-radius:3px;
+	background-color:#f6f6f6;			
+	margin:8px 0;
+	/*resize: vertical | horizontal | none | both*/
+	resize:none;
+	display:block;
+}
+    </style>
 </head>
 <body>
 	<h2>Editar Artículo</h2>
@@ -73,8 +132,8 @@
         @enderror
 
         <br><br>
-        <input type="button" onclick="history.back()" name="Cancelar" value="Cancelar">
-        <button type="submit" onclick="return ConfirmEdit()">Actualizar datos</button>
+        <input id="cancel" type="button" onclick="history.back()" name="Cancelar" value="Cancelar">
+        <button id="ok" type="submit" onclick="return ConfirmEdit()">Actualizar datos</button>
 
 
     </form>
